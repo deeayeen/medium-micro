36cc7cd62e52c1f7126192deb917e52284e28b67
Dan Halperin <dee.aye.en@gmail.com>
2019-11-08 17:52:34 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Reward from "react-rewards";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.clickMe = this.clickMe.bind(this);
+  }
+  clickMe() {
+    this.reward.rewardMe();
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Reward
+            ref={ref => {
+              this.reward = ref;
+            }}
+            type="confetti"
+          >
+            <p
+              style={{ cursor: "pointer", textDecoration: "underline" }}
+              className="App-link"
+              onClick={() => this.clickMe()}
+            >
+              Confetti!
+            </p>
+          </Reward>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
